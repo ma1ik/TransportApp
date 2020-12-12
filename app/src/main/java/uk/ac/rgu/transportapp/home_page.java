88aadd4677dbcb98344caa9068ff2c5245518f71
Abdulmalik <hazardgamin9@gmail.com>
2020-12-12 22:21:57 +0000

    Removed the journey page as no longer required

--- app/src/main/java/uk/ac/rgu/transportapp/home_page.java
+++ app/src/main/java/uk/ac/rgu/transportapp/home_page.java
@@ -21,8 +21,7 @@ public class home_page extends AppCompatActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        // add click listeners for the journey button, local bus stop button, timetable button and saved locations button
-        ((Button) findViewById(R.id.journeyButton)).setOnClickListener(this);
+        // add click listeners for the local bus stop button, timetable button and saved locations button
         ((Button) findViewById(R.id.localBusButton)).setOnClickListener(this);
         ((Button) findViewById(R.id.busTimeTableButton)).setOnClickListener(this);
         ((Button) findViewById(R.id.savedLocationsButton)).setOnClickListener(this);
@@ -82,10 +81,7 @@ public class home_page extends AppCompatActivity implements View.OnClickListener
     @Override
     public void onClick(View view) {
         // this method for onClick is for each button on the homepage to navigate to each page
-        if (view.getId() == R.id.journeyButton) {
-            Intent intent = new Intent(getApplicationContext(), journey.class);
-            startActivity(intent);
-        } else if (view.getId() == R.id.localBusButton){
+        if (view.getId() == R.id.localBusButton){
             Intent intent = new Intent(getApplicationContext(), local_bus_stops.class);
             startActivity(intent);
         } else if (view.getId() == R.id.busTimeTableButton){
